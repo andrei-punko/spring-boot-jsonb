015ab7f331e8be8b4f27ed9fe629da128ef95d9c
Andrei Punko <andd3dfx@gmail.com>
2021-03-12 15:37:35 +0300

    Add separate type for entity field instead of just String representation of object

---
+++ src/main/java/by/andd3dfx/templateapp/persistence/entities/Location.java
@@ -0,0 +1,13 @@
+package by.andd3dfx.templateapp.persistence.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Location {
+    private String country;
+    private String city;
+}
