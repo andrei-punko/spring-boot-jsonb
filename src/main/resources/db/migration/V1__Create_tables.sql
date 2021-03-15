1a60b7bde1c6f9cf59db4199bfa613accbbd587f
Andrei Punko <andd3dfx@gmail.com>
2021-03-15 10:51:40 +0300

    Add index for JSONB column of Postgres DB

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -6,7 +6,7 @@ create table articles (
     ts timestamp,
     text varchar not null,
     author varchar not null,
-    location JSONB null,
+    location JSONB,
     date_created timestamp not null,
     date_updated timestamp not null,
     primary key (id)
@@ -15,5 +15,8 @@ create table articles (
 create sequence article_id_seq START WITH 1 INCREMENT BY 1;
 
 create index article_title_idx on articles(title);
-create index article_date_created_idx on articles(date_created);
 create index article_ts_idx on articles(ts);
+create index article_date_created_idx on articles(date_created);
+
+-- according to: https://www.2ndquadrant.com/en/blog/jsonb-type-performance-postgresql-9-4/
+create index article_location_idx on articles using gin (location jsonb_path_ops);
