f5f75637b283dfbaef079d66da1e7eb752701694
Andrei Punko <andd3dfx@gmail.com>
2024-06-08 14:37:34 +0300

    Avoid mapper warnings about unmapped target

--- src/main/java/by/andd3dfx/templateapp/mappers/ArticleMapper.java
+++ src/main/java/by/andd3dfx/templateapp/mappers/ArticleMapper.java
@@ -7,8 +7,11 @@ import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ArticleMapper {
 
     ArticleDto toArticleDto(Article article);
